330b53aa0482d8ec8b87cef5c74ef08b99bb5ab2
night1308 <night1308@gmail.com>
2011-05-13 23:05:03 +0200

    add build for async server

--- async_client_server/build.mk
+++ async_client_server/build.mk
@@ -16,5 +16,23 @@ generated/client:
 target_async_client: generated/client
 
 
+## SERVER ##
+TARGETS+=server
+
+server_rule=c
+server_autodepsources=server.c
+server_cc=gcc -Wall -Werror -Wextra -Wshadow -Wpointer-arith -Wcast-qual -Wstrict-prototypes -Wmissing-prototypes
+server_cpp=$(client_cc) -Wnon-virtual-dtor
+server_ld=gcc
+server_strip=strip
+server_sourcesearchdirs=async_client_server
+server_headersearchdirs=async_client_server
+server_dynamiclibs=event
+
+generated/server:
+
+target_async_server: generated/server
+
+
 ## EXTRA ##
-target_async_client_server: target_async_client
+target_async_client_server: target_async_client target_async_server
